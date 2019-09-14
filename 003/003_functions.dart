067cd3442eb7dc5d638467f6c5a13561ac75df9c
kwakuadjei <duncanadjei@gmail.com>
2019-09-14 05:26:29 +0000

    functions in dart

--- 003/003_functions.dart
+++ 003/003_functions.dart
@@ -2,18 +2,26 @@
 
 // Implementing a function
 
-printFullName(){
+  String basicInfo(int age){ return "Return type"; }
+// ^^^^     ^^^^    ^^^^            ^^^^
+// Type     Name    Params     Statements & Expressions
 
-  String name = "John Doe";
+// Function with no return type or parameter(s)
+void privateKey(){}
 
-  print(name);
+// Functions with one expression can
+// be written as:
+bool isYoung() => 18>20;
+
+// Ommitted type
+printName(){
+  print("Dart Language");
 }
-
 
 // Where the app executes from is a function
+// This is the main function
 void main(){
-
-  // calling a function
-  printFullName();
-  
+  // Calling a function
+  printName();
 }
+
